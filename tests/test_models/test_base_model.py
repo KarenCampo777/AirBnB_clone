496861deb5c61125728128681f3481efd544067e
AndrewB4y <andresbaymon@gmail.com>
2020-06-30 21:20:30 +0000

    Adding test_base_model.py

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+""" Unittest module for base_model module """
+
+import unittest
+
+
+class TestInitBaseModel(unittest.TestCase):
+    """ Testing the initialization of a BaseModel instance """
+
+    def setUp(self):
+        my_base1 = BaseModel()
+
+    def test_create(self):
+        self.
